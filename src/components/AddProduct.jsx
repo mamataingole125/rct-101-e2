32860f026fdfcddf1986e6209ca1970a436cc6cc
mamataingole125 <mamataingole125@gmail.com>
2022-05-30 15:32:19 +0530

    add

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import {React,useState,useEffect} from "react";
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import axios from "axios"
 
 const AddProduct = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Button = () => <div />;
+  // const Button = () => <div />;
+  
   const Modal = () => <div />;
   const ModalBody = () => <div />;
   const Input = () => <div />;
@@ -10,24 +13,60 @@ const AddProduct = () => {
   const RadioGroup = () => <div />;
   const Radio = () => <div />;
 
+  const [todos,setTodos]=useState([])
+  const [page,setPage]=useState(1)
+  const [limit,setLimit]=useState(3)
+  const [newtodo,setNewtodo]=useState()
+  
+useEffect(()=>{
+  axios.get(`http://localhost:8080/products?_page=${page}&_limit=${limit}`)
+  .then((res)=>{
+    console.log(res.data)
+  })
+})
+
+
+
+
+fetch("http://localhost:8080/products",{
+  method:"POST",
+  headers:{
+    "content-type":"application/json"
+  },
+//   body:JSON.stringify({
+//     title: ,
+//     category: ,
+//     gender: ,
+//     imageSrc: ,
+//     price: 
+//   })
+})
+.then((res)=>res.json())
+.then((data)=>{
+  // setNewtodo(data)
+  console.log(data)
+})
+
+
   return (
     <>
-      <Button my={4} data-cy="add-product-button"></Button>
+      <Button my={4} data-cy="add-product-button">AddProduct</Button>
+      
       <Modal>
         <ModalBody pb={6}>
-          <Input data-cy="add-product-title" />
+          <Input data-cy="add-product-title"  />
           <Select data-cy="add-product-category">
-            <option data-cy="add-product-category-shirt"></option>
-            <option data-cy="add-product-category-pant"></option>
-            <option data-cy="add-product-category-jeans"></option>
+            <option data-cy="add-product-category-shirt">shirt</option>
+            <option data-cy="add-product-category-pant">pant</option>
+            <option data-cy="add-product-category-jeans">jeans</option>
           </Select>
           <RadioGroup data-cy="add-product-gender">
-            <Radio data-cy="add-product-gender-male"></Radio>
-            <Radio data-cy="add-product-gender-female"></Radio>
-            <Radio data-cy="add-product-gender-unisex"></Radio>
+            <Radio data-cy="add-product-gender-male">Male</Radio>
+            <Radio data-cy="add-product-gender-female">Female</Radio>
+            <Radio data-cy="add-product-gender-unisex">Unisex</Radio>
           </RadioGroup>
           <Input data-cy="add-product-price" />
-          <Button data-cy="add-product-submit-button"></Button>
+          <Button data-cy="add-product-submit-button">submit</Button>
         </ModalBody>
       </Modal>
     </>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import AddProduct from "./AddProduct"
+import Pagination from "./Pagination"
+
+
 
 const Products = () => {
+
+  
+
+
   // TODO: Remove below const and instead import them from chakra
   const Flex = () => <div />;
   const Grid = () => <div />;
 
+
+
+
   return (
     <Flex>
       {/*  AddProduct */}
+      <AddProduct/>
       <Grid>{/* List of Products */}</Grid>
       {/* Pagination */}
+      <Pagination/>
     </Flex>
   );
 };
